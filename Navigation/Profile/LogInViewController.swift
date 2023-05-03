e287c9df069b2510d7331d74be90dcc8bcc882e2
Stanislav <mr.dorrin91@gmail.com>
2023-05-03 23:11:12 +0300

    Домашние задание по теме «UIScrollView, UITableView, UITableViewDatasource, UITableViewDelegate», часть 1 --- v1

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -1,12 +1,36 @@
 import UIKit
 
+// Домашнее задание "UITableView, UIScrollView, datasource и delegate" часть 1
+// Уважаемый преподаватель, к сожалению у меня не получилось сделать задание на 100% - не могу понять, как связать UIScrollView с контентом
+// Подскажите пожалуйста, как нужно настроить, чтобы контент (который я отрисовал) отобразился в UIScrollView (для этого нужно мою верстку положить в UIView а его уже в UIScrollView?)
+//P.S. У меня был большой перерыв в обучении (полгода), не все смог вспомнить из пройденного - буду благодарен обратной связи
+
 class LogInViewController: UIViewController {
-  //comment
+    
+    private lazy var MyScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.backgroundColor = .systemBrown
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private lazy var contentView: UIView = {
+        let contentView = UIView()
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.backgroundColor = .systemBlue
+        
+        return contentView
+    }()
+    
     private lazy var LogoImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.clipsToBounds = true
         imageView.image = UIImage(named: "logo.jpeg")
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        
         return imageView
     }()
     
@@ -21,6 +45,7 @@ class LogInViewController: UIViewController {
         stackView.layer.borderColor = UIColor.lightGray.cgColor
         stackView.layer.borderWidth = 0.5
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        
         return stackView
     }()
     
@@ -42,11 +67,14 @@ class LogInViewController: UIViewController {
         let input = UITextField()
         input.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: input.frame.height))
         input.leftViewMode = .always
+        input.textColor = .black
+        input.font = UIFont.systemFont(ofSize: 16.0)
+        input.font = UIFont.boldSystemFont(ofSize: 10)
+        input.placeholder = "Password"
+        input.autocapitalizationType = .none
+        input.isSecureTextEntry = true
         input.layer.borderColor = UIColor.lightGray.cgColor
         input.layer.borderWidth = 0.5
-        input.textColor = .black
-        input.placeholder = "Password"
-        input.isSecureTextEntry = true
         input.translatesAutoresizingMaskIntoConstraints = false
         
         return input
@@ -74,15 +102,24 @@ class LogInViewController: UIViewController {
         self.view.addSubview(self.editButton)
         self.view.addSubview(self.MyStackView)
         
+        self.view.addSubview(self.MyScrollView)
+        self.view.addSubview(self.contentView)
+        
         let logoImageHeaderView = LogoImageHeaderView()
         let inputBlockForLoginHeaderView = InputBlockForLoginHeaderView()
         let inputBlockForPasswordHeaderView = InputBlockForPasswordHeaderView()
         let editButtonView = editButtonView()
         let myStackView = myStackView()
+        //let myScrollView = myScrollView()
+        //let myContentView = myContentView()
         
         self.MyStackView.center = self.view.center
         self.MyStackView.addArrangedSubview(self.inputBlockForLogin)
         self.MyStackView.addArrangedSubview(self.inputBlockForPassword)
+        
+        //contentView.addSubview(MyStackView)
+        //self.MyScrollView.addSubview(self.contentView)
+
         
         //активация констрейна
         NSLayoutConstraint.activate(logoImageHeaderView)
@@ -90,8 +127,29 @@ class LogInViewController: UIViewController {
         NSLayoutConstraint.activate(inputBlockForLoginHeaderView)
         NSLayoutConstraint.activate(inputBlockForPasswordHeaderView)
         NSLayoutConstraint.activate(editButtonView)
+        //NSLayoutConstraint.activate(myScrollView)
+    }
+    /*
+    private func myContentView() -> [NSLayoutConstraint]{
+        let topContraint = self.LogoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 10)
+        let heightAnchor = self.LogoImageView.heightAnchor.constraint(equalToConstant: 100)
+        let centerXAnchor = self.LogoImageView.centerXAnchor.constraint(equalTo: super.view.centerXAnchor)
+        
+        return [
+            topContraint, heightAnchor, centerXAnchor
+        ]
     }
     
+    private func myScrollView() -> [NSLayoutConstraint]{
+        let topContraint = self.LogoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 10)
+        let heightAnchor = self.LogoImageView.heightAnchor.constraint(equalToConstant: 100)
+        let centerXAnchor = self.LogoImageView.centerXAnchor.constraint(equalTo: super.view.centerXAnchor)
+        
+        return [
+            topContraint, heightAnchor, centerXAnchor
+        ]
+    }
+    */
     private func LogoImageHeaderView() -> [NSLayoutConstraint]{
         let topContraint = self.LogoImageView.topAnchor.constraint(equalTo: super.view.topAnchor, constant: 120)
         let widthAnchor = self.LogoImageView.widthAnchor.constraint(equalToConstant: 100)
@@ -149,6 +207,15 @@ class LogInViewController: UIViewController {
     @objc private func didTapButton(){
         let vc = PostViewController()
         self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func willShowKeyboard(_ notification: NSNotification) {
+        let keyboardHeight = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height
+        MyScrollView.contentInset.bottom += keyboardHeight ?? 0.0
+    }
+    
+    @objc func willHideKeyboard(_ notification: NSNotification) {
+        MyScrollView.contentInset.bottom = 0.0
     }
 }
 
